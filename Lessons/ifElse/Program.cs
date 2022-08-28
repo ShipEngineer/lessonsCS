e5e4bbe47344dd0254381bf080fe710430d51858
PoeticCrane <poeticcrane@gmail.com>
2022-08-28 21:39:08 +0300

    If Else

---
+++ Lessons/ifElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Put UserName: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "maria")
+{
+    Console.WriteLine("Yappy, Maria!");
+}
+else
+{
+    Console.Write("Hello common " + username);
+}
